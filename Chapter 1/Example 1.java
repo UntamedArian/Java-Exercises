7c8082697b8ad02c6255bbafbc213d70b35e4163
UntamedArian <skandakumar@outlook.com>
2018-06-21 16:08:57 +0200

    Newly created files

---
+++ Chapter 1/Example 1.java
@@ -0,0 +1,13 @@
+/* This is a simple Java Program
+File Name - Example 1.Java
+*/
+
+class Example
+{
+    // Your program begins with a call to main()
+    public static void main(String args [])
+    {
+        System.out.println("This is a simple Java Program - My first Hello World !!!");
+    }
+
+}
