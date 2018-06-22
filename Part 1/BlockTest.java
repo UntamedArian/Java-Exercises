4430c9aedbd147c95ce0eef711ed03a2d2d2aa11
UntamedArian <skandakumar@outlook.com>
2018-06-22 10:39:25 +0200

    BlockTesting

---
+++ Part 1/BlockTest.java
@@ -0,0 +1,20 @@
+/* This is to showcase the functionality of block codes in Java
+
+Filename would be BlockTest.java
+
+*/
+
+class BlockTest
+{
+    public static void main (String args[])
+    {
+        int x, y;
+        y=20;
+        for (x=0;x<10;x++)
+        {
+            System.out.println("This is X - " + x);
+            System.out.println("This is y - " + y);
+            y = y-2;
+        }
+    }
+}
