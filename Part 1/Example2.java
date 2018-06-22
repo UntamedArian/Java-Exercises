ac5a375e8700b69af1e6a3f249481ae96e9e4ede
UntamedArian <skandakumar@outlook.com>
2018-06-22 10:25:16 +0200

    Renamed Folders

---
+++ Part 1/Example2.java
@@ -0,0 +1,17 @@
+/* Here is another short example
+We will define a variable here
+File name would be - Example2.java
+*/
+
+class Example2
+{
+    public static void main (String args[])
+    {
+        int num; // here i defined an integar named num
+        num = 100;
+        System.out.println("This is num - " + num);
+        num = num * 2;
+        System.out.println("This is num multiplied by two and then rewrote the value back to num");
+        System.out.println(num);
+    }
+}
---
+++ Part 1/ForTest.java
@@ -0,0 +1,14 @@
+/* Demonstration of the FOR loop
+
+File name would be - ForTest.java
+
+*/
+
+class ForTest {
+    public static void main (String[] args)
+    {
+        int x;
+        for (x=0; x<10; x=x+1)
+            System.out.println("This is X : " + x);
+    }
+}
