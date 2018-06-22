da81b5136511cda645dbb2c299b6d2274f1ddf4c
UntamedArian <skandakumar@outlook.com>
2018-06-22 10:23:10 +0200

    For Test

--- Chapter 1/ForTest.java
+++ Chapter 1/ForTest.java
@@ -5,7 +5,7 @@ File name would be - ForTest.java
 */
 
 class ForTest {
-    public static void main (String args[])
+    public static void main (String[] args)
     {
         int x;
         for (x=0; x<10; x=x+1)
